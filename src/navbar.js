283f5512a3128bf04176d3c6b6b0b15c00a47810
Nat Sharpe <sharpe1890@gmail.com>
2018-09-23 15:39:58 -0400

    Navbar gets categories from state and shows links

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 let Navbar = props => (
-    <h1>Navbar!</h1>
+    <div>
+        <a href="/">Home</a>
+        <br/>
+        { 
+            props.categories.map(category =>
+            <p key={category.id}>
+            <Link to={`/categories/${category.id}`}>{category.title}</Link>
+            </p>    
+            )
+        }
+    </div>
 );
 
-export default Navbar;
+export default connect( (state) => ({categories : state.categories}) )(Navbar);
